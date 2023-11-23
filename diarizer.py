89a7a38b9a8eb45307788e03783e0c43c800ba98
ADVAGA1 <vaqueradriag@gmail.com>
2023-11-23 15:49:57 +0100

    diarizer

--- diarizer.py
+++ diarizer.py
@@ -3,13 +3,17 @@ from pyannote.audio import Pipeline
 TOKEN = "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"
 
 pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization@2.1",
-                                    use_auth_token=TOKEN)
+                                   use_auth_token=TOKEN)
 
-#pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization-3.0",
-#                                    use_auth_token=TOKEN)
 
+#pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization-3.1",
+#                                  use_auth_token=TOKEN)
+
+#import torch
+#pipeline.to(torch.device('cuda'))
 
 # apply the pipeline to an audio file
 diarization = pipeline("ejemplo2.wav", num_speakers=2)
 
-print(type(diarization))
+for turn, _, speaker in diarization.itertracks(yield_label=True):
+    print(f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}")
