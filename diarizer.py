f1e095d85e4d1cd70fbbf42258131a04bc744e64
ADVAGA1 <vaqueradriag@gmail.com>
2023-11-23 17:34:30 +0100

    diarization made with all the analysis, we should change the emotion analysis model

--- diarizer.py
+++ diarizer.py
@@ -13,7 +13,14 @@ pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization@2.1",
 #pipeline.to(torch.device('cuda'))
 
 # apply the pipeline to an audio file
-diarization = pipeline("ejemplo2.wav", num_speakers=2)
+def diarization(file):
+    diarization = pipeline(file, num_speakers=2)
 
-for turn, _, speaker in diarization.itertracks(yield_label=True):
-    print(f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}")
+    for turn, track, speaker in diarization.itertracks(yield_label=True):
+        print(turn)
+        print(f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}")
+
+
+
+if __name__ == "__main__":
+    diarization("ejemplo2.wav")
