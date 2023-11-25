e27f7ebf8543570792b263410ffd724dbfe52f34
matedavid <david.mate.lopez@gmail.com>
2023-11-25 15:31:22 +0100

    started api and put pipeline into function

--- api.py
+++ api.py
@@ -1,49 +1,15 @@
-from transformers import pipeline
+from flask import Flask
+import sentia 
 
-whisper = pipeline("automatic-speech-recognition",
-                   model="openai/whisper-base", chunk_length_s=30, batch_size=16)
+app = Flask(__name__)
 
-model_path = "cardiffnlp/twitter-xlm-roberta-base-sentiment"
-sentiment = pipeline("sentiment-analysis",
-                     model=model_path, tokenizer=model_path)
+@app.route("/")
+def home():
+    return "<p>Hello world</p>"
 
-
-def process_audio(filepath: str) -> dict[str, float]:
-
-    chunks = whisper(filepath, batch_size=16, return_timestamps=True)["chunks"]
-
-    sentiments = []
-
-    for chunk in chunks:
-        s = sentiment(chunk["text"])[0]
-        s["text"] = chunk["text"]
-        sentiments.append(s)
-
-    result = {}
-
-    sum = 0
-
-    for s in sentiments:
-        label = s["label"]
-        score = s["score"]
-
-        if label == 'negative':
-            score = -score
-        if label == 'neutral':
-            score = 0
-
-        print(s["text"], score)
-
-        if label == 'neutral':
-            score = 0
-
-        sum = sum + score
-
-    result["label"] = "positive" if sum >= 0 else "negative"
-    result["score"] = sum
-
-    return result
-
+@app.route("/audio")
+def audio():
+    return sentia.process("ejemplo2.wav")
 
 if __name__ == "__main__":
-    print(process_audio("positive.opus"))
+    app.run(debug=True)
